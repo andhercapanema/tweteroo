9546ed8fae8ee79846a86f49ead78e5f3854b99f
andherpaulo <andher.dev@gmail.com>
2022-11-07 09:58:22 -0300

    Adição da funcionalidade de login

--- index.js
+++ index.js
@@ -2,6 +2,24 @@ import express from "express";
 import cors from "cors";
 
 const app = express();
+
 app.use(cors());
+app.use(express.json());
+
+const users = [];
+
+app.post("/sign-up", (req, res) => {
+    const { username, avatar } = req.body;
+
+    const newUser = { username, avatar };
+
+    users.push(newUser);
+
+    res.status(200).send("OK");
+});
+
+app.get("/tweets", (req, res) => {
+    console.log("deu bom");
+});
 
 app.listen(5000);
